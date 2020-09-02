943e0f3cc8e6212e3b500f400e1332e1caf5f1ce
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-09-02 21:56:57 +0900

    BodySkin test
    js consol test

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -3,12 +3,21 @@ totalLinks = links.length;
 
 function setActiveStyle(color) {
   for (let i = 0; i < totalLinks; i++) {
-    if (color == links[i].getAttribute("title")) {
+    if (color === links[i].getAttribute("title")) {
       links[i].removeAttribute("disabled");
     } else {
       links[i].setAttribute("disabled", "true");
     }
   }
+}
+
+// body Skin
+const bodySkin = document.querySelectorAll(".body-skin"),
+  totalBodySkin = bodySkin.length;
+for (let i = 0; i < totalBodySkin; i++) {
+  bodySkin[i].addEventListener("change", function () {
+    console.log(this.value);
+  });
 }
 
 document
