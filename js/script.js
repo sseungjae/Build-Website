03f3c9e36f51734e2a635f27af5f3cfd8e70fb17
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-09-04 00:16:30 +0900

    js elemenst test
    consol test

--- js/script.js
+++ js/script.js
@@ -40,7 +40,12 @@ for (let i = 0; i < totalNavList; i++) {
     for (let j = 0; j < totalNavList; j++) {
       navList[j].querySelector("a").classList.remove("active");
     }
-
     this.classList.add("active");
+
+    showSection(this);
   });
+}
+
+function showSection(element) {
+  console.log(element.getAttribute("href"));
 }
