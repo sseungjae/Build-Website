e7ccdc616060e0b9a6aebe1317b64cef18caca4e
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-09-03 23:43:52 +0900

    js test
    checked Elements

--- js/script.js
+++ js/script.js
@@ -33,6 +33,14 @@ const nav = document.querySelector(".nav");
 navList = nav.querySelectorAll("li");
 totalNavList = navList.length;
 
+//a 태그를 가진 nav 리스트를 전부 부를 수 있다 a href=""
 for (let i = 0; i < totalNavList; i++) {
-  console.log(navList[i]);
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let j = 0; j < totalNavList; j++) {
+      navList[j].querySelector("a").classList.remove("active");
+    }
+
+    this.classList.add("active");
+  });
 }
