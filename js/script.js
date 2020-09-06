1b7f9cf6e88fcfac8f5e2423f36799c5d5b5b52e
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-09-06 14:45:25 +0900

    preloader completed
    loading page end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+window.addEventListener("load", function () {
+  document.querySelector(".preloader").classList.add("opacity-0");
+
+  setTimeout(function () {
+    document.querySelector(".preloader").style.display = "none";
+  }, 1000);
+});
+
 // Portfolio Item Filter
 
 const filterContainer = document.querySelector(".portfolio-filter"),
